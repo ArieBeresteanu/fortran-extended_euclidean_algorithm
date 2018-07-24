f4a9b9a2d20f3750bccb0dd8f7083e9e6033c43f
Albert R. Carnier Guedes <albert@teko.net.br>
2018-07-23 23:41:02 -0300

    Update main.f95

--- main.f95
+++ main.f95
@@ -7,13 +7,13 @@
 !
       PROGRAM EXTENDED_EUCLIDEAN_ALGORITHM
 
-      integer p,q,m,n
+            integer p,q,m,n
       
-      read*, p,q
+            read*, p,q
 
-      call EEA(p,q,m,n)
+            call EEA(p,q,m,n)
 
-      print*, m,n
+            print*, m,n
       
       END PROGRAM EXTENDED_EUCLIDEAN_ALGORITHM
 
@@ -27,50 +27,50 @@
 !
       SUBROUTINE EEA(p,q,m,n)
 
-      integer p,q,m,n
-      integer t,x,y,z0,z1,w0,w1
-      integer temp
+            integer p,q,m,n
+            integer t,x,y,z0,z1,w0,w1
+            integer temp
       
-      x=p
-      y=q
+            x=p
+            y=q
       
 !     Verify if 'p' is lower than 'q'
-      if(p.lt.q) then
-         x=q
-         y=p
-      end if
+            if(p.lt.q) then
+                  x=q
+                  y=p
+            end if
 
-      z0=1
-      z1=0
+            z0=1
+            z1=0
       
-      w0=0
-      w1=1
+            w0=0
+            w1=1
       
 !     Iteratively calculates the remainder of the division of 'x' by 'y'.
-      do while( y.ne.0 )
+            do while( y.ne.0 )
 
-         r = modulo(x,y)
+                  r = modulo(x,y)
 
 !     't' is the module of 'x = r + t*y'
-         t = (x-r)/y
+                  t = (x-r)/y
 
 !     Renew 'x' and 'y' with new values.
-         x = y
-         y = r
+                  x = y
+                  y = r
 
 !     Apply recursive formula for EEA to find 'm'
-         temp = z1
-         z1 = z0 - t*z1
-         z0 = temp
+                  temp = z1
+                  z1 = z0 - t*z1
+                  z0 = temp
 
 !     Apply recursive formula for EEA to find 'n'         
-         temp = w1
-         w1 = w0 - t*w1
-         w0 = temp
+                  temp = w1
+                  w1 = w0 - t*w1
+                  w0 = temp
                   
-      enddo
+            enddo
 
-      m=z0
-      n=w0
+            m=z0
+            n=w0
       
       END SUBROUTINE EEA
